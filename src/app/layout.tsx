087bacf040bf9e2bdadb6d4a5405d979b5f0c50f
Christian Martins <cmts2010@gmail.com>
2023-09-09 00:34:16 -0300

    feat: front start

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Money Manager: Seu gerenciador financeiro!',
+  description: 'Faça o controle de suas finanças de forma simples e prática!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
